57a816cad3323e9978941033dd136d3cbd00095e
Tsukasa <jordan.rouco-miguez@epitech.eu>
2017-11-01 14:02:20 +0100

    Version windows ajoutée

---
+++ windows/include/UPDATER.h
@@ -0,0 +1,16 @@
+#ifndef UPDATER_H
+# define UPDATER_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+
+typedef struct datas
+{
+	float version;
+	float nversion;
+	char *site;
+}	datas;
+
+#endif
